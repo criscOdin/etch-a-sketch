831f7ce701159e9701d31aeaab26b0b6ca6e4058
criscOdin <ciscol64@hotmail.com>
2022-04-13 23:43:20 -0500

    request for number of squares and paint them

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-const grid = document.querySelector('.grid');
-const width = Math.floor(document.body.clientWidth / 16);
-const height = Math.floor(document.body.clientHeight / 16);
+function createGrid(numberOfSquares) {
+  const grid = document.querySelector('.grid');
+  const width = Math.floor(document.body.clientWidth / numberOfSquares);
+  const height = Math.floor(document.body.clientHeight / numberOfSquares);
 
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement('div');
-  row.classList.add('row');
-  grid.appendChild(row);
-  for (let j = 0; j < 16; j++) {
-    let div = document.createElement('div');
-    div.classList.add('square');
-    console.log(width);
-    div.setAttribute('style', `width: ${width}px; height: ${height}px`);
-    div.style.height = height;
-    div.addEventListener('mouseover', () => {
-      div.classList.add('blue');
-    });
-    row.appendChild(div);
+  for (let i = 0; i < numberOfSquares; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    grid.appendChild(row);
+    for (let j = 0; j < numberOfSquares; j++) {
+      let div = document.createElement('div');
+      div.classList.add('square');
+      console.log(width);
+      div.setAttribute('style', `width: ${width}px; height: ${height}px`);
+      div.style.height = height;
+      div.addEventListener('mouseover', () => {
+        div.classList.add('blue');
+      });
+      row.appendChild(div);
+    }
   }
 }
 
 const clear = document.querySelector('.clear');
-const divs = document.querySelectorAll('.square');
 clear.addEventListener('click', () => {
+  const divs = document.querySelectorAll('.square');
   divs.forEach((e) => {
     e.classList.remove('blue');
   });
 });
+
+let numberOfSquares = prompt('Please indicate the number of squares per side');
+if (numberOfSquares <= 100) {
+  createGrid(numberOfSquares);
+} else {
+  alert('number of Squares should be less than 100');
+}
