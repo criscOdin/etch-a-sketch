510816a359e9d78ae74b31c6ed3fd0d39d0f6de9
criscOdin <ciscol64@hotmail.com>
2022-04-13 23:34:13 -0500

    auto width and height, button for clear

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const grid = document.querySelector('.grid');
+const width = Math.floor(document.body.clientWidth / 16);
+const height = Math.floor(document.body.clientHeight / 16);
+
 for (let i = 0; i < 16; i++) {
   let row = document.createElement('div');
   row.classList.add('row');
@@ -6,9 +9,20 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let div = document.createElement('div');
     div.classList.add('square');
+    console.log(width);
+    div.setAttribute('style', `width: ${width}px; height: ${height}px`);
+    div.style.height = height;
     div.addEventListener('mouseover', () => {
       div.classList.add('blue');
     });
     row.appendChild(div);
   }
 }
+
+const clear = document.querySelector('.clear');
+const divs = document.querySelectorAll('.square');
+clear.addEventListener('click', () => {
+  divs.forEach((e) => {
+    e.classList.remove('blue');
+  });
+});
